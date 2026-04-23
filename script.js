edfa997a32153d26c828c3a000305e3adca0e894
Sakshi Ghanwat <141915486+SakshiGhanwat@users.noreply.github.com>
2026-04-23 15:13:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+ const form = document.getElementById("form");
+
+    form.addEventListener("submit", function(e) {
+      e.preventDefault(); // prevent page reload
+
+      const age = document.getElementById("age").value;
+      const name = document.getElementById("name").value;
+
+      // Validation
+      if (age === "" || name === "") {
+        alert("Please enter valid details.");
+        return;
+      }
+
+      // Promise logic
+      const checkAge = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (age > 18) {
+            resolve(`Welcome, ${name}. You can vote.`);
+          } else {
+            reject(`Oh sorry ${name}. You aren't old enough.`);
+          }
+        }, 4000);
+      });
+
+      // Handle promise
+      checkAge
+        .then((message) => {
+          alert(message);
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    });
